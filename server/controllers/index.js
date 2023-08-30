77fa1e94942e29ed171be717cbeab998310b9000
Justin Sanchez <justinlsanchez12@gmail.com>
2023-08-30 01:10:19 -0700

    added searchResults routes

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,9 @@
 //const albumRoutes = require('./albumRoutes');
 const artistRoutes = require('./artistRoutes');
 //const songRoutes = require('./songRoutes');
+const searchRoutes = require('./searchResultsRoutes');
 
 module.exports = {
     artistRoutes,
+    searchRoutes
 };
