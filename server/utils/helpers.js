3342b27d9d1afb7ac4e95a676e0c69a4255839c1
Justin Sanchez <justinlsanchez12@gmail.com>
2023-08-30 01:13:02 -0700

    middleware functions

---
+++ server/utils/helpers.js
@@ -0,0 +1,9 @@
+function logRequest(req, res, next) {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+  }
+  
+  module.exports = {
+    logRequest,
+  };
+  
