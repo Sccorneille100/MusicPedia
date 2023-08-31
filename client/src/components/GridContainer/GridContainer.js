c349686dc43683bcbed3ea9a5cde5585777b04f4
Sccorneille100 <127679758+Sccorneille100@users.noreply.github.com>
2023-08-31 11:37:12 -0500

    Delete client directory

--- client/src/components/GridContainer/GridContainer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import './grid-container.scss'
-
-const GridContainer = ({ children }) => {
-  return <div className='grid-container'>{children}</div>
-}
-
-export default GridContainer
