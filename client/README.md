0ca23b1b64b559d76fa801f61cf54590930aa566
Sylvestre corneille <Sylvestrecorneille@icloud.com>
2023-08-31 12:32:53 -0500

    tj

---
+++ client/README.md
@@ -0,0 +1,32 @@
+## Getting Started
+
+First, clone the [spotify graphql server](https://github.com/alis-khadka/spotify-graphql-server) :
+
+required node version: v8.17.0 (I tried to run using latetest version but could not.)
+
+run the graphql server
+
+```bash
+npm start
+```
+
+Then, run the development server (client-side):
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:4200](http://localhost:4200) with your browser to see the result.
+
+## Test
+
+```bash
+npm test
+```
+
+### Note: Make sure the graphql server is running.
+I tried using mocks but it was taking more time to get it right.
+
+## Live Demo
+
+[Demo](https://spotify-graphql-client.vercel.app/)
---
+++ client/test/pages/_app.test.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { render, fireEvent, act } from '@testing-library/react';
+import preloadAll from 'jest-next-dynamic';
+import singletonRouter from 'next/router';
+import mockRouter from 'next-router-mock';
+
+import MyApp from '../../pages/_app';
+import App from '../../pages/index';
+
+jest.setTimeout(100000);
+
+jest.mock('next/dist/client/router', () => require('next-router-mock'));
+
+beforeEach(() => {
+  mockRouter.setCurrentUrl('/');
+});
+
+beforeAll(() => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+});
+
+test('should search for the provided query', async () => {
+  await act(async () => {
+    await preloadAll();
+    const { container } = render(<MyApp Component={App} />);
+
+    const searchField = container.querySelector('#search-field');
+    expect(searchField).toBeInTheDocument();
+
+    fireEvent.change(searchField, { target: { value: 'best songs' } });
+    const searchBtn = container.querySelector('.ant-input-search-button');
+    fireEvent.click(searchBtn);
+    await new Promise((r) => setTimeout(r, 5000));
+
+    expect(
+      container.querySelector('.ant-page-header-heading-title').innerHTML
+    ).toBe('Results for: best songs');
+  });
+});
+
+test('should navigate to the specific playlist page when clicked on an playlist from the results', async () => {
+  await act(async () => {
+    await preloadAll();
+    const { container } = render(<MyApp Component={App} />);
+
+    await new Promise((r) => setTimeout(r, 4000));
+
+    const playlistCard = container.querySelector(
+      '[rowkey="5ABHKGoOzxkaa28ttQV9sE"]'
+    );
+    fireEvent.click(playlistCard);
+    await new Promise((r) => setTimeout(r, 4000));
+
+    expect(singletonRouter).toMatchObject({
+      asPath: '/playlists/5ABHKGoOzxkaa28ttQV9sE',
+    });
+  });
+});
